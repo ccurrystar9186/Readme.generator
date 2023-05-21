8c40adfe3a1eec211103a21a63e37491c650d2f7
ccurrystar9186 <36233138+ccurrystar9186@users.noreply.github.com>
2023-05-21 17:54:08 -0500

    Update createReadme.js

--- Utility/createReadme.js
+++ Utility/createReadme.js
@@ -1,4 +1,4 @@
-function createReadme(data) { //this template literal takes data from the index.js file and uses is to write the ReadMe. The "#" are supposed to be used to set headings but they don't look right in the actual file ¯\_(ツ)_/¯
+function createReadme(data) { //this template show  data from the index.js file and uses is to write the ReadMe. The "#" are to the headings but looks indifferent then the actual file ¯\_(ツ)_/¯
     return `# ${data.title}
     ## Purpose
     ${data.description}
